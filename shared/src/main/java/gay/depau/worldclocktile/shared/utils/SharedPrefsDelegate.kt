838573fbbcf37e8f803a9303be811aebb4e872ff
Davide Depau <davide@depau.eu>
2023-12-28 17:46:13 +0100

    Allow upgrading sharedprefs int to long

--- shared/src/main/java/gay/depau/worldclocktile/shared/utils/SharedPrefsDelegate.kt
+++ shared/src/main/java/gay/depau/worldclocktile/shared/utils/SharedPrefsDelegate.kt
@@ -28,7 +28,7 @@ class SharedSettingsPropertyDelegate<SettingsType : SettingsChangeNotifier, Retu
     val marshal: (ReturnType) -> SharedPrefsType,
     val unmarshal: (SharedPrefsType) -> ReturnType = {
         it as ReturnType
-    }
+    },
 ) {
     operator fun getValue(thisRef: SettingsType, property: KProperty<*>) =
         when (sharedPrefsTypeClass) {
@@ -38,50 +38,71 @@ class SharedSettingsPropertyDelegate<SettingsType : SettingsChangeNotifier, Retu
                     marshal(defaultValue) as String?
                 ) as SharedPrefsType
             )
+
             Int::class -> unmarshal(
                 sharedPrefs.getInt(
                     key,
                     marshal(defaultValue) as Int
                 ) as SharedPrefsType
             )
+
             Boolean::class -> unmarshal(
                 sharedPrefs.getBoolean(
                     key,
                     marshal(defaultValue) as Boolean
                 ) as SharedPrefsType
             )
+
             Float::class -> unmarshal(
                 sharedPrefs.getFloat(
                     key,
                     marshal(defaultValue) as Float
                 ) as SharedPrefsType
             )
-            Long::class -> unmarshal(
-                sharedPrefs.getLong(
-                    key,
-                    marshal(defaultValue) as Long
-                ) as SharedPrefsType
-            )
+
+            Long::class -> {
+                try {
+                    unmarshal(
+                        sharedPrefs.getLong(
+                            key,
+                            marshal(defaultValue) as Long
+                        ) as SharedPrefsType
+                    )
+                } catch (e: ClassCastException) {
+                    // Allow upgrading from Int to Long
+                    unmarshal(
+                        sharedPrefs.getInt(
+                            key,
+                            (marshal(defaultValue) as Long).toInt()
+                        ) as SharedPrefsType
+                    )
+                }
+            }
+
             Set::class -> unmarshal(
                 sharedPrefs.getStringSet(
                     key,
                     marshal(defaultValue) as Set<String>?
                 ) as SharedPrefsType
             )
+
             else -> throw IllegalArgumentException("Unsupported type")
         }
 
     operator fun setValue(thisRef: SettingsType, property: KProperty<*>, value: ReturnType) {
         sharedPrefs.edit().apply {
-            when (sharedPrefsTypeClass) {
-                String::class -> putString(key, marshal(value) as String)
-                Int::class -> putInt(key, marshal(value) as Int)
-                Boolean::class -> putBoolean(key, marshal(value) as Boolean)
-                Float::class -> putFloat(key, marshal(value) as Float)
-                Long::class -> putLong(key, marshal(value) as Long)
-                Set::class -> putStringSet(key, marshal(value) as Set<String>)
-                else -> throw IllegalArgumentException("Unsupported type")
-            }
+            if (value == null)
+                remove(key)
+            else
+                when (sharedPrefsTypeClass) {
+                    String::class -> putString(key, marshal(value) as String)
+                    Int::class -> putInt(key, marshal(value) as Int)
+                    Boolean::class -> putBoolean(key, marshal(value) as Boolean)
+                    Float::class -> putFloat(key, marshal(value) as Float)
+                    Long::class -> putLong(key, marshal(value) as Long)
+                    Set::class -> putStringSet(key, marshal(value) as Set<String>)
+                    else -> throw IllegalArgumentException("Unsupported type")
+                }
             apply()
             thisRef.notifyListeners()
         }
@@ -94,7 +115,7 @@ inline fun <SettingsType : SettingsChangeNotifier, ReturnType, reified SharedPre
     key: String,
     defaultValue: ReturnType,
     noinline unmarshal: (SharedPrefsType) -> ReturnType,
-    noinline marshal: (ReturnType) -> SharedPrefsType
+    noinline marshal: (ReturnType) -> SharedPrefsType,
 ): SharedSettingsPropertyDelegate<SettingsType, ReturnType, SharedPrefsType> {
     return SharedSettingsPropertyDelegate(
         SharedPrefsType::class,
@@ -111,7 +132,7 @@ inline fun <SettingsType : SettingsChangeNotifier, reified T> SharedPreferences.
     key: String,
     defaultValue: T,
     noinline unmarshal: (T) -> T = { it },
-    noinline marshal: (T) -> T = { it }
+    noinline marshal: (T) -> T = { it },
 ) = delegate<SettingsType, T, T>(key, defaultValue, unmarshal, marshal)
 
 @JvmName("enumDelegate")
@@ -119,5 +140,5 @@ inline fun <SettingsType : SettingsChangeNotifier, reified T : Enum<T>> SharedPr
     key: String,
     defaultValue: T,
     noinline unmarshal: (String) -> T = { enumValueOf(it) },
-    noinline marshal: (T) -> String = { it.name }
+    noinline marshal: (T) -> String = { it.name },
 ) = delegate<SettingsType, T, String>(key, defaultValue, unmarshal, marshal)
